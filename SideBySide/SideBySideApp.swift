84fec81faeaccebd11cea0dc65f12c9b9346b0d0
Karen Tran <karentran28@hotmail.com>
2025-11-22 23:52:59 -0800

    Initial Commit

---
+++ SideBySide/SideBySideApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideBySideApp.swift
+//  SideBySide
+//
+//  Created by Karen Tran on 2025-11-22.
+//
+
+import SwiftUI
+
+@main
+struct SideBySideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
